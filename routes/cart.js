ee9b3dab209cf5d3f2081df76efe23ac57e31828
cuananarjayca-source <cuananarjay.ca@gmail.com>
2026-04-24 21:46:21 +0800

    Fixed Argument error

--- routes/cart.js
+++ routes/cart.js
@@ -10,9 +10,9 @@ router.post("/add-to-cart", verify, cartController.addToCart);
 router.patch("/update-cart-quantity", verify, cartController.changeProductQuantity);
 
 // Additional cart functionalities
-router.post("/remove-from-cart", verify, cartController.removeFromCart);
-router.post("/clear-cart", verify, cartController.clearCartItems);
-router.post("/search-item", verify, cartController.searchProductByName);
-router.post("/search-item-by-pricerange", verify, cartController.searchProductByPriceRange);
+// router.post("/remove-from-cart", verify, cartController.removeFromCart);
+// router.post("/clear-cart", verify, cartController.clearCartItems);
+// router.post("/search-item", verify, cartController.searchProductByName);
+// router.post("/search-item-by-pricerange", verify, cartController.searchProductByPriceRange);
 
 module.exports = router;
