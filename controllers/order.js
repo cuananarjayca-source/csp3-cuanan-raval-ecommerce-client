9577949a1444353f820aa4405f2578f7f7bbd595
cuananarjayca-source <cuananarjay.ca@gmail.com>
2026-04-28 19:14:24 +0800

    Added changes in order controller and routes
    Declared Cart import, made sure it returns the cartitems

--- controllers/order.js
+++ controllers/order.js
@@ -1,62 +1,60 @@
 const User = require("../models/User");
 const Order = require("../models/Order");
 const Product = require("../models/Product");
+const Cart = require("../models/Cart");
 const { errorHandler } = require("../auth");
 
 
 module.exports.createOrder = (req, res) => {
-	const { productId, quantity, subtotal } = req.body;
-
-	return Order.findOne({ userId: req.user.id })
-	.then(order=> {
-		if (!order) {
-			const newOrder = new Order({
-				userId: req.user.id,
-				productsOrdered: [{ productId, quantity, subtotal }],
-				totalPrice: subtotal
-			});
-			return newOrder.save()
-			.then(result=> res.status(201).send({
-				message: "Items ordered successfully",
-				result: result
-			}));
+	return Cart.findOne({ userId: req.user.id })
+	.then(cart => {
+		if (!cart || cart.cartItems.length === 0) {
+			return res.status(400).send({ error: "No Items to Checkout" });
 		}
-
-	order.productsOrdered.push({ productId, quantity, subtotal});
-	order.totalPrice += subtotal; 
-
-	return order.save()
-	.then(result=> res.status(200).send({
-		message: "Items ordered successfully",
-		result: result
+		const orderItems = cart.cartItems.map(item => ({ //
+			productId: item.productId,
+			quantity: item.quantity,
+			subtotal: item.subtotal 
+		}));
+		const totalPrice = cart.totalPrice;
+		const order = new Order({
+			userId: req.user.id,
+			productsOrdered: orderItems,
+			totalPrice: totalPrice
+		});
+		return order.save()
+		.then(result => res.status(201).send({
+			message: "Ordered successfully"
 		}));
 	})
-	.catch(err=> errorHandler(err,req,res));
-};	
-
-module.exports.getUserOrder = (req, res) => {
-    return Order.find({ userId: req.user.id })
-    .then(order => {
-        if (order.length === 0) {
-            return res.status(404).send({ message: "No order history found"})
-            };
-            return newOrder.save()
-            .then(savedOrder => res.status(200).send({
-                order: order
-            }));
-        })
-    .catch(err => errorHandler(err, req, res));
+	.catch(err => errorHandler(err, req, res));
 };
 
-module.exports.getAllOrders= (req, res) => {
+
+module.exports.getUserOrder = (req, res) => {
+	return Order.find({ userId: req.user.id })
+	.then(result => {
+		if (result.length === 0) {
+			return res.status(404).send({ error: "No orders found" });
+		}
+		return res.status(200).send({
+			orders: result
+		});
+	})
+	.catch(err => errorHandler(err, req, res));
+};
+
+
+// Admin can view all orders
+module.exports.getAllOrders = (req, res) => {
+
 	return Order.find()
 	.then(result => {
 		if (result.length === 0) {
 			return res.status(404).send({ message: "No orders found"});
 		}
 		return res.status(200).send({
-			message: "Orders found",
-			result: result
+			orders: result
 		});
 	})
 	.catch(err=> errorHandler(err, req, res));	
--- routes/order.js
+++ routes/order.js
@@ -4,15 +4,11 @@ const { verify, verifyAdmin } = require("../auth");
 const router = express.Router();
 
 // Authenticated user access only
-
-
-router.post("/create-order", verify, orderController.createOrder);
-
+router.post("/checkout", verify, orderController.createOrder);
 router.get("/my-orders", verify, orderController.getUserOrder);
 
 // ADMIN ONLY
-
-router.get("/get-all-orders", verify, verifyAdmin, orderController.getAllOrders);
+router.get("/all-orders", verify, verifyAdmin, orderController.getAllOrders);
 
 
 module.exports = router;
